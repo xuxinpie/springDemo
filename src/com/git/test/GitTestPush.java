7bb3e5183e3767f7dd847903cc1fd657d965b0cb
Xinux <xuxinpie@users.noreply.github.com>
2015-03-31 10:49:03 +0800

    Update GitTestPush.java

--- src/com/git/test/GitTestPush.java
+++ src/com/git/test/GitTestPush.java
@@ -2,7 +2,7 @@ package com.git.test;
 
 public class GitTestPush {
     public static void main(String[] args) {
-        System.out.println("This is for test git push use!");
+        System.out.println("This demo is for test git push use!");
     }
 
 }
