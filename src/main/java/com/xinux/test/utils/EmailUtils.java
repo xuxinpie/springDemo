e291fc0867efe3f54daf32be2130c5fbb1ae50ff
xuxinpie <xuxinpie@gmail.com>
2015-04-26 20:06:27 +0800

    add EmailUtils

--- src/main/java/com/xinux/test/utils/EmailUtils.java
+++ src/main/java/com/xinux/test/utils/EmailUtils.java
@@ -5,23 +5,26 @@
 
 package com.xinux.test.utils;
 
-import org.apache.commons.lang.StringUtils;
 import java.util.regex.Pattern;
+
+import org.apache.commons.lang.StringUtils;
 
 /**
  * Email parsing/validation utilities.
  *
- * @author vkolodrevskiy
+ * @author Xinux
  */
 public final class EmailUtils {
-	/**
-	 * Returns true if the String is a valid email address.
-	 */
-	public static boolean isEmail(String string) {
+    /**
+     * Returns true if the String is a valid email address.
+     */
+    public static boolean isEmail(String string) {
         return !StringUtils.isEmpty(string) && emailPattern.matcher(string).matches();
     }
 
-	private static final Pattern emailPattern = Pattern.compile("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}");
+    private static final Pattern emailPattern = Pattern
+                                                  .compile("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}");
 
-	private EmailUtils() { }
+    private EmailUtils() {
+    }
 }
